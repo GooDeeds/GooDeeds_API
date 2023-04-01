7e7bfd45faf6f8522ba05d1ec63676ed92a1df8d
YoungFlyme <youngflyme@web.de>
2023-04-01 23:41:44 +0200

    added deed_generator field from DB

--- GooDeeds_API/Deed.cs
+++ GooDeeds_API/Deed.cs
@@ -6,5 +6,6 @@
         public string Title { get; set; }
         public string Description { get; set; }
         public int Experience { get; set; }
+        public int Deed_generator { get; set; }
     }
 }
