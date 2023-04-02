7821d4d52bb971aeb7670b8682448f9ee432da19
YoungFlyme <youngflyme@web.de>
2023-04-02 16:11:45 +0200

    added achievements

---
+++ GooDeeds_API/Achievement.cs
@@ -0,0 +1,11 @@
+﻿namespace GooDeeds_API
+{
+    public class Achievement
+    {
+        public int Id { get; set; }
+        public int Value { get; set; }
+        public int Type { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ GooDeeds_API/Controllers/AchievementController.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using System.Xml.Linq;
+
+namespace GooDeeds_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]/{id?}")]
+    public class AchievementController : Controller
+    {
+        private MySqlConnection _connection;
+
+        public AchievementController(MySqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        [HttpGet(Name = "GetAchievement")]
+        public IEnumerable<Achievement> Get(int? id)
+        {
+            if (id.HasValue)
+            {
+                return _connection.Query<Achievement>("select * from achievements where Id = @Id", new { Id = id });
+            }
+            else
+            {
+                return _connection.Query<Achievement>("select * from achievements");
+            }
+        }
+    }
+}
